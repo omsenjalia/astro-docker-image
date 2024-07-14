b0ed64b45c1fd5617eac82ffadab737705a8b267
om <omsenjalia@gmail.com>
2024-07-14 14:07:45 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ cd /home/container
 yarn astro --info ## only really needed to show what version is being used. Should be changed for different applications
 
 # Replace Startup Variables
-# MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-# echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=/bin/bash
 
 # Run the Server
-# ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
